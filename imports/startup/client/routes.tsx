9f3b63df4873e02eff73eb7391f1d5f24ac12494
igor abramenkov <igorek699669@gmail.com>
2020-07-15 13:57:27 +0300

    #frontend- add mygroups filtration

--- imports/startup/client/routes.tsx
+++ imports/startup/client/routes.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { mount } from 'react-mounter';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+import { Users } from "../../api/users";
+import { Meteor } from 'meteor/meteor';
 import { GroupsPage } from '../../ui/pages/Groups';
 import { GroupPage } from '../../ui/pages/Group';
 import { AddGroupPage } from '../../ui/pages/AddGroup';
 import {useSubscription} from "../../lib/client/reactHooks";
+import {useTracker} from "meteor/react-meteor-data";
 
 const MainLayout = ({ content }) => {
     useSubscription('users.all');
+    const loginUser = ()=>{
+        new Promise((resolve, reject) => {
+            let dataP = useTracker(() => Users.find({_id: '666'}).fetch());
+            return resolve(dataP)
+        }).then((data)=>{
+            Meteor.loginWithPassword(data[0].username, '111111');
+        });
+    };
+
     return (
         <div>
             <header>
                 This is our header;
+                {Meteor.user()===null?(
+                    <button onClick={()=>loginUser}>Залогиниться</button>
+                ): <button>Вы уже залогинены</button>}
+
             </header>
             <main>
                 {content()}
--- imports/ui/pages/Groups/index.tsx
+++ imports/ui/pages/Groups/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import './style.less';
 import { Group } from "../../components/Group";
@@ -7,16 +7,15 @@ import { Users } from "../../../api/users";
 import  {Meteor} from 'meteor/meteor';
 import { useSubscription } from '../../../lib/client/reactHooks';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { Accounts } from 'meteor/accounts-base';
 
 export const GroupsPage = () => {
+    const [myGroupsView, setMyGroupsView] = useState(0);
     useSubscription('groups.all');
 
+
     const groups = useTracker(() => Groups.find({}, { sort: { createdAt: -1 } }).fetch(), []);
-    const myGroups = useTracker(()=>Groups.find({}, { owner: Meteor.user()}).fetch(), []);
-    setTimeout(function () {
-        console.log(Meteor.user());
-    }, 5000);
+    const myGroups = useTracker(() => Groups.find({owner: Meteor.user()?Meteor.user()._id:null}, { sort: { createdAt: -1 } }).fetch(), []);
+
     const renderGroups = () => {
         return groups.map((group) => {
             return (
@@ -27,6 +26,19 @@ export const GroupsPage = () => {
             );
         });
     };
+    const renderMyGroups = ()=>{
+        if(Meteor.user()!==null){
+            return myGroups.map((group) => {
+                return (
+                    <Group
+                        key={group._id}
+                        group={group}
+                    />
+                );
+            });
+        }
+        return 'Вы не залогинены';
+    };
 
     return (
         <>
@@ -34,16 +46,23 @@ export const GroupsPage = () => {
                 <div className="container">
                     <div className="groups-tabs">
                         <div className="tab-buttons-wrapper">
-                            <button className="tab-buttons-wrapper__button">Все сообщества</button>
-                            <button className="tab-buttons-wrapper__button">Мои сообщества</button>
+                            <button onClick={()=>setMyGroupsView(0)} className="tab-buttons-wrapper__button">Все сообщества</button>
+                            <button onClick={()=>setMyGroupsView(1)} className="tab-buttons-wrapper__button">Мои сообщества</button>
                             <button onClick={()=>{
                                 FlowRouter.go('addGroup')
                             }} className="tab-buttons-wrapper__button">Создать</button>
                         </div>
                         <input type="text" className="search-input" placeholder='Поиск сообществ...' />
-                        <div className="tab-content all-groups">
-                            {renderGroups()}
-                        </div>
+                        {myGroupsView==0? (
+                            <div className="tab-content all-groups">
+                                {renderGroups()}
+                            </div>
+                        ):(
+                            <div className="tab-content my-groups">
+                                {renderMyGroups()}
+                            </div>
+                        )}
+
                     </div>
                 </div>
             </section>
